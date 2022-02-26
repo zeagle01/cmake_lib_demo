6648f2c22f3c77ed6b3f9b667561449b73bc78c0
zeagle <zeagle01@163.com>
2022-02-26 15:31:14 +0800

    remove cuda

--- A/B/B-config.cmake
+++ A/B/B-config.cmake
@@ -1,6 +1,5 @@
 include(CMakeFindDependencyMacro)
 
 find_dependency(D)
-include(FindCUDAToolkit)
 
 include("${CMAKE_CURRENT_LIST_DIR}/B_targets.cmake")
--- A/B/b.cpp
+++ A/B/b.cpp
@@ -1,11 +1,7 @@
 
 #include "b.h"
 #include "D/d.h"
-#include "curand.h"
 #include <iostream>
-#include <curand_kernel.h>
-#include <curand.h>
-#include <cufft.h>
 
 void B::greet()
 {
@@ -13,22 +9,6 @@ void B::greet()
     d.greet();
     printf("i'm b\n");
 
-    greet_from_kernel();
-
-
-
-
-	cufftHandle plan;
-	cufftComplex* data;
-
-#define NX 256 
-#define BATCH 10
-
-	cudaMalloc((void**)&data,
-		sizeof(cufftComplex) * NX * BATCH);
-	/* Create a 1D FFT plan.  */
-
-	cufftPlan1d(&plan, NX, CUFFT_C2C, BATCH);
 }
 
 
